f816938049a83dc1ee18778b2eca15df8d2285de
Manasi Sanjay Jadhav <101620713+manasisj2001@users.noreply.github.com>
2024-03-03 22:12:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wildfire_detection_system
+ 
---
+++ app.py
@@ -0,0 +1,137 @@
+from __future__ import division, print_function
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import hashlib
+import re
+  
+# coding=utf-8
+from keras.models import load_model
+from keras.preprocessing import image
+
+import sys
+import os
+import glob
+import re
+import numpy as np
+import random
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+import model
+
+  
+app = Flask(__name__)
+  
+  
+app.secret_key = 'xyzsdfg'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user-system'
+  
+mysql = MySQL(app)
+
+
+
+
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+  
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return render_template('predict.html', mesage = mesage)
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage = mesage)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+  
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password, ))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
+
+
+
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        tempr=random.randint(50,101)
+        fire = model.predict(file_path)
+        if fire:
+            if tempr>70: 
+                prediction = 'fire.......'           
+            else:
+                prediction = 'fire and smoke.......'
+        else:
+            prediction = 'no fire'
+
+        return prediction
+       
+  
+    return None
+
+if __name__ == "__main__":
+    app.run()
